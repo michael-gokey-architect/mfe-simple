bad8e5a6b5eec34e91496cb4f2219abf1b5fbb0c
michael-gokey-architect <michael.gokey.architect@gmail.com>
2023-01-22 16:04:11 -0500

    attempting first build

---
+++ projects/products/src/index.js
@@ -0,0 +1,12 @@
+import faker from 'faker';
+
+// https://fakerjs.dev/api/
+
+let products = '';
+
+for (let i = 0 ; i < 6; i++){{
+    const name = faker.commerce.productName();
+    product += `<div>${name}</div>`;
+}
+ console.log(products);
+
